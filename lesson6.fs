b287d21258ac9b85fd7747378f08d9a2c3d01e5f
olegnizamov <35757313+olegnizamov@users.noreply.github.com>
2021-10-15 18:49:31 +0400

    урок 8 - функциональное программирование F#

---
+++ lesson6.fs
@@ -0,0 +1,29 @@
+// 17.1
+let rec pow =
+    function
+    | (char, 0) -> ""
+    | (char, n) -> char + pow (char, n - 1)
+//printfn "%s" (pow ("a", 5))
+
+
+// 17.2
+let rec isIthChar =
+    function
+    | (s, n, c) -> (string s).[n] = c
+
+//printfn "%b" (isIthChar ("aaac", 3,'d'))
+
+
+// 17.3
+let rec occFromIth (str,number,char)=
+    (let rec iterator =
+        function
+        | (s, n, c, count) when ((String.length s) <= n) -> count
+        | (s, n, c, count) when  ((string s).[n] = c) -> iterator(s,n+1,c,count+1)
+        | (s, n, c, count) when  ((string s).[n] <> c) -> iterator(s,n+1,c,count)
+        | _ -> 0
+
+     iterator (str, number, char, 0))
+
+
+//printfn "%d" (occFromIth ("abcabcabc", 0,'a'))
