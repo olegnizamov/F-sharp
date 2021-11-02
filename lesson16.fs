8c9dee18032c4b4829a99ed5a1044241309bc02f
Олег Низамов <35757313+olegnizamov@users.noreply.github.com>
2021-11-02 12:49:52 +0400

    урок 18 - функциональное программирование F#

---
+++ lesson16.fs
@@ -0,0 +1,11 @@
+// 42.3
+let rec allSubsets n k =
+    let rec powerset xs=
+        match xs with
+        | [] -> [[]]
+        | head::tail -> List.fold (fun head1 tail1 -> (head::tail1)::tail1::head1) [] (powerset tail)
+    Set.filter (fun xs -> Set.count xs = k) (Set.ofList (List.map Set.ofList (powerset [1 .. n])))
+
+
+//let l = (allSubsets 10 5)
+//let a =1
